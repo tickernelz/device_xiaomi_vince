f6f264d36ba8c86ba556e23f1e213d46eaaa0332
Isaac Chen <isaacchen@isaacchen.cn>
2018-04-04 14:23:11 +0800

    vince: Apply lineage vendor rebrand
    Signed-off-by: Isaac Chen <isaacchen@isaacchen.cn>
    
    # Conflicts:
    #	lineage.mk

--- lineage.mk
+++ lineage.mk
@@ -18,7 +18,7 @@
 $(call inherit-product, device/xiaomi/vince/full_vince.mk)
 
 # Inherit some common LineageOS stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 PRODUCT_NAME := lineage_vince
 BOARD_VENDOR := Xiaomi
