6eac82aa04a26f2ba0be9619807d5bc7a14aafd9
zhafron123 <zhafronadani@gmail.com>
2018-04-04 23:55:54 +0700

    vince: change KERNEL_CONFIG
    Signed-off-by: zhafron123 <zhafronadani@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -147,7 +147,7 @@ BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
 BOARD_KERNEL_IMAGE_NAME := Image.gz-dtb
 BOARD_KERNEL_PAGESIZE :=  2048
 BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x01000000 --tags_offset 0x00000100
-TARGET_KERNEL_CONFIG := msm_defconfig
+TARGET_KERNEL_CONFIG := lineageos_vince_defconfig
 TARGET_KERNEL_SOURCE := kernel/xiaomi/msm8953
 TARGET_USE_SDCLANG := true
 
